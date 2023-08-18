b24a1105afec9fe4445d65acc2fc39a239d76edc
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-08-18 22:27:17 +0300

    add text animations to navLinks

--- components/NavLinks/NavLinks.tsx
+++ components/NavLinks/NavLinks.tsx
@@ -7,22 +7,46 @@ export const NavLinks = async ({lang}: {lang: Locale}) => {
   const dictionary = await getDictionary(lang);
   return (
     <nav className={styles.wrap}>
-      <Link className={styles.link} href="">
+      <Link
+        className={styles.link}
+        href=""
+        data-item={dictionary["server-component"].links.home}
+      >
         {dictionary["server-component"].links.home}
       </Link>
-      <Link className={styles.link} href="">
+      <Link
+        className={styles.link}
+        href=""
+        data-item={dictionary["server-component"].links.technologies}
+      >
         {dictionary["server-component"].links.technologies}
       </Link>
-      <Link className={styles.link} href="">
+      <Link
+        className={styles.link}
+        href=""
+        data-item={dictionary["server-component"].links.projects}
+      >
         {dictionary["server-component"].links.projects}
       </Link>
-      <Link className={styles.link} href="">
+      <Link
+        className={styles.link}
+        href=""
+        data-item={dictionary["server-component"].links.ourTeam}
+      >
         {dictionary["server-component"].links.ourTeam}
       </Link>
-      <Link className={styles.link} href="">
+      <Link
+        className={styles.link}
+        href=""
+        data-item={dictionary["server-component"].links.services}
+      >
         {dictionary["server-component"].links.services}
       </Link>
-      <Link className={styles.link} href="">
+      <Link
+        className={styles.link}
+        href=""
+        data-item={dictionary["server-component"].links.contact}
+      >
         {dictionary["server-component"].links.contact}
       </Link>
     </nav>
