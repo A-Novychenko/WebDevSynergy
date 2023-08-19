f93a4c3664f68afc1d45d1d19de890046dc93325
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-08-20 01:31:41 +0300

    fix

--- components/localeSwitcher/LocaleSwitcher.tsx
+++ components/localeSwitcher/LocaleSwitcher.tsx
@@ -2,13 +2,13 @@
 
 import {usePathname} from "next/navigation";
 import Link from "next/link";
-import {i18n} from "../../i18n-config";
+import {Locale, i18n} from "../../i18n-config";
 import styles from "./LocaleSwitcher.module.css";
 
 export default function LocaleSwitcher() {
   const pathName = usePathname();
   console.log("first", typeof pathName.slice(1, 3));
-  const redirectedPathName = (locale: string) => {
+  const redirectedPathName = (locale: Locale) => {
     if (!pathName) return "/";
     const segments = pathName.split("/");
     segments[1] = locale;
