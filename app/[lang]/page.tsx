3f9516e5bbde5a61c27bed57061faf4495584fef
KharlanEd <eduardxarlan@gmail.com>
2023-09-05 18:17:52 +0300

    Start Skills

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,14 +1,15 @@
-import {getDictionary} from "../../get-dictionary";
-import {Locale} from "../../i18n-config";
-import {Hero} from "@/components/Sections/Hero/Hero";
+import { getDictionary } from "../../get-dictionary";
+import { Locale } from "../../i18n-config";
+import { Hero } from "@/components/Sections/Hero/Hero";
+import { Skills } from "@/components/Sections/Skills/Skills";
 
-export default async function Home({params: {lang}}: {params: {lang: Locale}}) {
+export default async function Home({ params: { lang } }: { params: { lang: Locale } }) {
   const dictionary = await getDictionary(lang);
   return (
     <>
       <Hero />
       <Hero />
-      <Hero />
+      <Skills />
       <Hero />
     </>
   );
---
+++ components/Sections/Skills/Skills.tsx
@@ -0,0 +1,18 @@
+import styled from './skills.module.scss'
+
+export const Skills = () => {
+
+
+    return (
+        <div className={styled.container}>
+            <section>
+                <h4>power our exceptional solutions</h4>
+                <h3>The skills, tools and technologies</h3>
+
+            </section>
+
+        </div>
+
+    )
+
+}
