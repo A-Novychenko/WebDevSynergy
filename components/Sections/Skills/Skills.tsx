4ecbdee22bc1404b91ab08dd7189615a9c6280ef
KharlanEd <eduardxarlan@gmail.com>
2023-09-05 20:11:25 +0300

    fix

--- components/Sections/Skills/Skills.tsx
+++ components/Sections/Skills/Skills.tsx
@@ -1,3 +1,4 @@
+import { title } from 'process'
 import styled from './skills.module.scss'
 
 export const Skills = () => {
@@ -6,8 +7,8 @@ export const Skills = () => {
     return (
         <div className={styled.container}>
             <section>
-                <h4>power our exceptional solutions</h4>
-                <h3>The skills, tools and technologies</h3>
+                <h4 className={styled.text_above_header}>power our exceptional solutions</h4>
+                <h3 className={styled.title}>The skills, tools and technologies</h3>
 
             </section>
 
