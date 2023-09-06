7f62c2432ab7ff5fe644a0c9540855a962cf08a8
KharlanEd <eduardxarlan@gmail.com>
2023-09-06 23:22:02 +0300

    start Skills mobile and tablet

--- components/Sections/Skills/Skills.tsx
+++ components/Sections/Skills/Skills.tsx
@@ -1,19 +1,12 @@
-'use client'
+
 import styled from './skills.module.scss'
 
-import Image from 'next/image'
-import { useState } from 'react';
 import { DataItem } from '@/data/skills';
+import { MobileItem } from '@/components/Elements/ItemSkills/MobileItemSkills';
+import { ItemSkills } from '@/components/Elements/ItemSkills/ItemSkills';
 
 export const Skills = ({ arraySkills }: { arraySkills: DataItem[] }) => {
 
-    const [showAll, setShowAll] = useState(false);
-
-    const toggleShowAll = () => {
-        setShowAll(!showAll);
-    };
-
-    const visibleItemCount = showAll ? arraySkills.length : 6;
 
 
 
@@ -23,25 +16,9 @@ export const Skills = ({ arraySkills }: { arraySkills: DataItem[] }) => {
                 <h4 className={styled.text_above_header}>power our exceptional solutions</h4>
                 <h3 className={styled.title}>The skills, tools and technologies</h3>
                 <p className={styled.subtitle}>At WebDevSynergy, projects aren't just about code – they're about crafting solutions that make a difference. Join us on this journey of excellence, innovation, and transformation.</p>
-                <ul className={styled.wrapper_card}>
-                    {arraySkills.slice(0, visibleItemCount).map((item) => {
-                        return (
 
-                            <li key={item.id} >
-                                <Image src={item.logo} width={40} height={40} alt={item.name} />
-                                <p>{item.name}</p>
-                            </li>
-
-                        )
-                    })}
-                </ul>
-                {arraySkills.length > 6 && (
-                    <div className={styled.show_more_button}>
-                        <button onClick={toggleShowAll}>
-                            {showAll ? 'Показать меньше' : 'Показать все элементы'}
-                        </button>
-                    </div>
-                )}
+                <MobileItem arraySkills={arraySkills} />
+                <ItemSkills arraySkills={arraySkills} />
             </section>
 
         </div >
