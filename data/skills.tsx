abe8408c35665400aff2c8a5553a6ec4c493a4d2
KharlanEd <eduardxarlan@gmail.com>
2023-09-06 23:22:11 +0300

    start Skills mobile and tablet

--- data/skills.tsx
+++ data/skills.tsx
@@ -10,6 +10,20 @@ import next from '../public/svg/next.js.svg';
 import nodejs from '../public/svg/nodejs.svg';
 import express from '../public/svg/express.svg';
 import mongoose from '../public/svg/mongoose.svg';
+import mongodb from '../public/svg/mongodb.svg';
+import git from '../public/svg/git.svg';
+import webpack from '../public/svg/webpack.svg';
+import parcel from '../public/svg/parcel.svg';
+import vite from '../public/svg/vite.svg';
+import sass from '../public/svg/icons8-sass 1.svg'
+import emotion from '../public/svg/emotion.svg';
+import styled from '../public/svg/styled components.svg';
+import tailwind from '../public/svg/tailwind css.svg';
+import docker from '../public/svg/docker.svg';
+import material from '../public/svg/material ui.svg';
+import figma from '../public/svg/figma.svg';
+
+
 
 export interface DataItem {
     id: number;
@@ -76,6 +90,66 @@ export const arraySkills: DataItem[] = [
         id: 12,
         logo: mongoose,
         name: 'mongoose',
+    },
+    {
+        id: 13,
+        logo: mongodb,
+        name: 'mongodb',
+    },
+    {
+        id: 14,
+        logo: git,
+        name: 'git',
+    },
+    {
+        id: 15,
+        logo: webpack,
+        name: 'webpack',
+    },
+    {
+        id: 16,
+        logo: parcel,
+        name: 'parcel',
+    },
+    {
+        id: 17,
+        logo: vite,
+        name: 'vite',
+    },
+    {
+        id: 18,
+        logo: sass,
+        name: 'sass',
+    },
+    {
+        id: 19,
+        logo: emotion,
+        name: 'emotion',
+    },
+    {
+        id: 20,
+        logo: styled,
+        name: 'styled',
+    },
+    {
+        id: 21,
+        logo: tailwind,
+        name: 'tailwind ',
+    },
+    {
+        id: 22,
+        logo: docker,
+        name: 'docker',
+    },
+    {
+        id: 23,
+        logo: material,
+        name: 'material ui',
+    },
+    {
+        id: 24,
+        logo: figma,
+        name: 'figma',
     },
 
 
