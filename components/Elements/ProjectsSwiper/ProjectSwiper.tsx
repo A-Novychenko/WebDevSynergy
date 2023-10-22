efc7719aefdd223a1cb35a82cfd7e3037fc1d645
Viacheslav Borysov <s.borisovbar@gmail.com>
2023-10-23 02:20:32 +0300

    add styles

--- components/Elements/ProjectsSwiper/ProjectSwiper.tsx
+++ components/Elements/ProjectsSwiper/ProjectSwiper.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import {Swiper, SwiperSlide} from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 import { useRef } from "react";
 
@@ -10,16 +10,17 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 
-import {projects} from "@/data/projects";
+import { projects } from "@/data/projects";
 import { Locale } from "@/i18n-config";
 
-import styles from './projectSwiper.module.scss'
+import styles from "./projectSwiper.module.scss";
 import NavLink from "next/link";
 
-export const ProjectSwiper = ({lang = "en"}: {lang: Locale}) => {
+import arrow from "../../../public/svg/arrow-btn.svg";
+
+export const ProjectSwiper = ({ lang = "en" }: { lang: Locale }) => {
   const slides: Project[] = projects;
   const swiperRef = useRef<any>(null);
-  
 
   console.log("lang", lang);
 
@@ -30,34 +31,73 @@ export const ProjectSwiper = ({lang = "en"}: {lang: Locale}) => {
         spaceBetween={24}
         slidesPerView={3}
         // navigation
-        pagination={false}
+        // pagination={false}
         // scrollbar={{draggable: true}}
         onSwiper={(swiper) => {
           swiperRef.current = swiper;
         }}
         onSlideChange={() => console.log("slide change")}
+        breakpoints={{
+        375: {
+          slidesPerView: 1,
+          pagination: true,
+        },
+        480: {
+          slidesPerView: 1,
+          pagination: true,
+        },
+        768: {
+          slidesPerView: 4,
+          slidesPerGroup: 4,
+        },
+        1280: {
+          slidesPerView: 3,
+          spaceBetween: 24,
+        },
+        }}
+        className={styles.swiper_wrapper}
         // style={{ width: "100%", height: "241px" }}
       >
         {slides &&
-          slides.map(({id, poster, title, discription, techIcon}) => {
+          slides.map(({ id, poster, title, discription, techIcon }) => {
             return (
-              
               <SwiperSlide key={id} className={styles.wrapper}>
-                <Image src={poster} alt={title} width={544} height={360} className={styles.image} />
+                <Image
+                  src={poster}
+                  alt={title}
+                  width={544}
+                  height={360}
+                  className={styles.image}
+                />
                 <p className={styles.title}>{title}</p>
                 <p className={styles.description}>{discription[lang]}</p>
-                <ul>
-                  {techIcon.map((icon, index) => { return (<li key={index}><Image  src={icon} alt={icon} width={24} height={24} /></li>) })}
-                  </ul>
-                <NavLink href="/" style={{color: "white"}}>Read more</NavLink>
+                <ul className={styles.skill_list}>
+                  {techIcon.map((icon, index) => {
+                    return (
+                      <li key={index}>
+                        <Image src={icon} alt={icon} width={24} height={24} />
+                      </li>
+                    );
+                  })}
+                </ul>
+                <NavLink
+                  href="/"
+                  style={{ color: "white" }}
+                  className={styles.nav_link}
+                >
+                  Read more
+                  <Image src={arrow} alt="arrow-icon" width={16} height={16} className={styles.arrow_icon} />
+                </NavLink>
               </SwiperSlide>
             );
           })}
       </Swiper>
-      <button type="button" onClick={() => swiperRef.current.slidePrev()}>Prev</button>
-      <button type="button" onClick={() => swiperRef.current.slideNext()}>
-      Next
+      <button type="button" onClick={() => swiperRef.current.slidePrev()}>
+        Prev
       </button>
-      </div>
+      <button type="button" onClick={() => swiperRef.current.slideNext()}>
+        Next
+      </button>
+    </div>
   );
 };
